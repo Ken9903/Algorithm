1c02e7a69f37b5f1557f1bc3c8cd63d8c7f8fe3a
Ken <127284612+Ken9903@users.noreply.github.com>
2023-04-30 23:12:01 +0900

    [Bronze V] Title: 단어 길이 재기, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Bronze/2743. 단어 길이 재기/단어 길이 재기.cc
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <algorithm>
+#include <string>
+#include <sstream>
+#include <vector>
+#include <utility>
+
+using namespace std;
+
+
+int main()
+{
+	string input;
+	getline(cin, input);
+
+	cout << input.length();
+
+	
+	
+
+
+
+}
