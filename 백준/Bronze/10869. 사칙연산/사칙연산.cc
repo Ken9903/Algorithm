d80f41417bc8622de676f4a5c4397dc746caaf3f
Ken <127284612+Ken9903@users.noreply.github.com>
2023-09-30 04:28:38 +0900

    [Bronze V] Title: 사칙연산, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/10869. 사칙연산/사칙연산.cc
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <deque>
+#include <algorithm>
+
+
+using namespace std;
+
+int main()
+{
+    int a, b;
+    cin >> a >> b;
+
+    cout << a + b << '\n';
+    cout << a - b << '\n';
+    cout << a * b << '\n';
+    cout << a / b << '\n';
+    cout << a % b << '\n';
+    
+    return 0;
+}
+
+
+
